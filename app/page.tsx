6c97c1dd19724146fca8a80ed717b9143ffedf9b
Hd-Lg <59037834+Hd-Lg@users.noreply.github.com>
2022-11-05 21:28:25 +0100

    Restructured folders and Suspense fragment

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
+import TodosList from "./(users)/todos/TodosList";
 
 type Props = {};
 
 function page({}: Props) {
-	return <div>page</div>;
+	return (
+		<div>
+			<Suspense fallback={<p>Loading the todos</p>}>
+				<h1>Loading todos</h1>
+				<div className="flex space-x-2">
+					{/* @ts-ignore */}
+					<TodosList />
+				</div>
+			</Suspense>
+			<Suspense fallback={<p>Loading trolley</p>}>
+				<h1>Loading shopping trolley</h1>
+				<div className="flex space-x-2">
+					{/* @ts-ignore */}
+					<TodosList />
+				</div>
+			</Suspense>
+		</div>
+	);
 }
 
 export default page;
